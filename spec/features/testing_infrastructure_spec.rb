cdee4956140db06a950de73a039e5e263f587c8c
Alex Sweeney <alexsweeney634@gmail.com>
2022-01-11 17:08:35 +0000

    fixed errors in adding player names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,8 +4,10 @@
 feature 'Enter player names' do
   scenario 'should request names for 2 players and retun them to page' do
     visit('/')
-    find_field('playerone').value
-    find_field('playertwo').value
+    fill_in :playerone, with: 'Alex'
+    fill_in :playertwo, with: 'Rosie'
+    click_button 'Submit'
+    expect(page).to have_content 'Welcome Alex and Rosie to battleships!' 
     
   end
 end
